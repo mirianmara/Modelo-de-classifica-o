7f31ce666e8f9c82aae89f2d4656b05c2fa9121c
Mirian Mara <mirian.marab@gmail.com>
2022-11-29 13:25:13 -0300

    Inserida legenda

--- OneDrive/Área de Trabalho/tese_at/Primeiro Commit/teste1.py
+++ OneDrive/Área de Trabalho/tese_at/Primeiro Commit/teste1.py
@@ -107,6 +107,7 @@ text = st.text ( '''Possibilidades de classificação, considerando o nível ót
 pela perspectiva do desempenho corporativo:
     
     
+    
     Práticas tributárias muito conservadoras;
     Práticas tributárias conservadoras;
     Práticas tributárias agressivas e
